0b83b66018da8c69c988ffbcc1f33ebc2bc771ed
yonglokkim <eureka7000@naver.com>
2022-03-07 14:54:02 +0900

    수정

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_assets = true
   config.serve_static_files = true
+  config.public_file_server.enabled = true
 
   #config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
