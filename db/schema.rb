3b46f1fd60f66b509b0d630e2487dc0a8b394fa6
yonglokkim <eureka7000@naver.com>
2022-03-18 10:02:26 +0900

    수정

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_06_073024) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_16_040510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_06_073024) do
     t.string "isbn"
     t.string "title"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
